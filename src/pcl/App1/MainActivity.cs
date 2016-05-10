88baff0149ff07d0b493cc6d76f34031e2b70e80
Qiming Liu <qooroo@gmail.com>
2016-05-11 00:52:28 +0100

    testing wip

---
+++ src/pcl/App1/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using WampSharp.V2;
+using WampSharp.V2.Client;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            Websockets.Droid.WebsocketConnection.Link();
+
+            var factory = new DefaultWampChannelFactory();
+
+            const string serverAddress = "ws://web-dev.adaptivecluster.com:8080/ws";
+
+            var channel = factory.CreateJsonChannel(serverAddress, "com.weareadaptive.reactivetrader");
+
+            channel.Open().Wait(5000);
+
+            var realmProxy = channel.RealmProxy;
+
+            var received = 0;
+            IDisposable subscription = null;
+
+            subscription =
+                realmProxy.Services.GetSubject<dynamic>("prices")
+                     .Subscribe(x =>
+                     {
+                         received++;
+
+                         if (received > 5)
+                         {
+                             subscription.Dispose();
+                         }
+                     });
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ src/pcl/ConsoleApplication1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WampSharp.V2;
+using WampSharp.V2.Client;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Websockets.Net.WebsocketConnection.Link();
+
+            DefaultWampChannelFactory factory =
+                new DefaultWampChannelFactory();
+
+            const string serverAddress = "ws://web-dev.adaptivecluster.com:8080/ws";
+
+            IWampChannel channel =
+                factory.CreateJsonChannel(serverAddress, "com.weareadaptive.reactivetrader");
+
+            channel.Open().Wait(5000);
+
+            IWampRealmProxy realmProxy = channel.RealmProxy;
+
+            int received = 0;
+            IDisposable subscription = null;
+
+            subscription =
+                realmProxy.Services.GetSubject<dynamic>("prices")
+                     .Subscribe(x =>
+                     {
+                         Console.WriteLine("Got Event: " + x);
+
+                         received++;
+
+                         if (received > 5)
+                         {
+                             Console.WriteLine("Closing ..");
+                             subscription.Dispose();
+                         }
+                     });
+
+            Console.ReadLine();
+        }
+    }
+}
